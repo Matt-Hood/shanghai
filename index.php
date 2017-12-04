5c0a2e4afdd82f3bf06dae9fa92dd4c71691cd39
fromero2 <32085702+fromero2@users.noreply.github.com>
2017-12-04 15:20:19 -0500

    adding audio code
    added audio code for song clips

--- index.php
+++ index.php
@@ -3,39 +3,39 @@ $currentTitle = "Assignment 8 - Linkin Park";
 include "inc/top.inc";
 ?>
 
-<!-- Navigation -->
-<nav class="main-menu">
-    <span class="logo"><a href="#">CSC174 Assignment #8</a></span>
-    <ul>
-        <li><span>Go to: </span></li>
-        <li><a class="slide" href="#summary">Summary</a></li>
-        <li><a class="slide" href="#survey">Survey</a></li>
-        <li><a class="slide" href="#credits">Credits</a></li>
-        <li><a href="user-account.php">User Account</a></li>
-    </ul>
-</nav>
+    <!-- Navigation -->
+    <nav class="main-menu">
+        <span class="logo"><a href="#">CSC174 Assignment #8</a></span>
+        <ul>
+            <li><span>Go to: </span></li>
+            <li><a class="slide" href="#summary">Summary</a></li>
+            <li><a class="slide" href="#survey">Survey</a></li>
+            <li><a class="slide" href="#credits">Credits</a></li>
+            <li><a href="user-account.php">User Account</a></li>
+        </ul>
+    </nav>
 
-<!-- Bio -->
-<div class="summary" id="summary">
-<div class="jumbotron">
-  <h1 class="display-3">LINKIN PARK OFFICIAL SITE</h1>
-    
-    <img src="https://up-1.cdn-fullscreendirect.com/production/photos/10272/original/20170502_173225_10272_989344.jpeg">
-    
-  <p class="lead">PRE ORDER ONE MORE LIGHT LIVE - AVAILABLE DECEMBER 15TH!</p>
-  <hr class="my-4">
-  <p>We decided to do our survery on Linkin Park because it is a band that we thought most people would be familiar with. In recent news, lead localist Chester Bennington died and we thought that this survey would pay tribute to him and the band.</p>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="https://www.bestbuy.com/site/one-more-light-live-cd/6165300.p?skuId=6165300&ref=212&loc=1&ksid=997fa4ba-1f82-4a1f-85ea-58a06b1224c0&ksprof_id=3&ksaffcode=pg218887&ksdevice=c&lsft=ref:212,loc:2&gclid=EAIaIQobChMI4LCBw-Lv1wIV1bjACh2KRQctEAQYASABEgJJh_D_BwE" role="button">PRE ORDER</a>
-  </p>
-</div>
-</div>
-<!--<div class="summary" id="summary">
+    <!-- Bio -->
+    <div class="summary" id="summary">
+        <div class="jumbotron">
+            <h1 class="display-3">LINKIN PARK OFFICIAL SITE</h1>
+
+            <img src="https://up-1.cdn-fullscreendirect.com/production/photos/10272/original/20170502_173225_10272_989344.jpeg">
+
+            <p class="lead">PRE ORDER ONE MORE LIGHT LIVE - AVAILABLE DECEMBER 15TH!</p>
+            <hr class="my-4">
+            <p>We decided to do our survery on Linkin Park because it is a band that we thought most people would be familiar with. In recent news, lead localist Chester Bennington died and we thought that this survey would pay tribute to him and the band.</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="https://www.bestbuy.com/site/one-more-light-live-cd/6165300.p?skuId=6165300&ref=212&loc=1&ksid=997fa4ba-1f82-4a1f-85ea-58a06b1224c0&ksprof_id=3&ksaffcode=pg218887&ksdevice=c&lsft=ref:212,loc:2&gclid=EAIaIQobChMI4LCBw-Lv1wIV1bjACh2KRQctEAQYASABEgJJh_D_BwE" role="button">PRE ORDER</a>
+            </p>
+        </div>
+    </div>
+    <!--<div class="summary" id="summary">
     <h2>Summary</h2>
     <div id="summary" class="row align-items-center">
         <div class="column col-md-4 col-sm-4 col-xs-12">
             <!-- Image from http://www.billboard.com/articles/columns/rock/7325484/linkin-park-2016-album-charity-poker-tournament-interview -->
-            <!--<img src="images/band.jpg" class="bandpic" alt="Linkin_Park">
+    <!--<img src="images/band.jpg" class="bandpic" alt="Linkin_Park">
         </div>
 
         <div class="column col-md-8 col-sm-8 col-xs-12">
@@ -46,187 +46,196 @@ include "inc/top.inc";
     </div>
 </div>-->
 
-<!-- End Bio -->
+    <!-- End Bio -->
 
-<!-- Albums -->
-<div class="albums" id="albums">
-    <h2>Albums</h2>
+    <!-- Albums -->
+    <div class="albums" id="albums">
+        <h2>Albums</h2>
 
-    <!-- Hybrid Theory -->
-    <div class="row align-items-center">
-        <div class="column col-md-4 col-sm-4 col-xs-12">
-            <!-- Image from https://en.wikipedia.org/wiki/Hybrid_Theory -->
-            <img src="images/hybrid_theory.jpg" alt="Hybrid_Thoery_Art">
-        </div>
-
-        <div class="column col-md-8 col-sm-8 col-xs-12">
-            <h3>Hybrid Theory</h3>
-            <p>Hybrid Theory is the debut studio album by American rock band Linkin Park, released on October 24, 2000 through Warner Bros. Records. As of 2017, the album has been certified diamond by the RIAA for sales in the United States of over eleven million units, and peaking at number two on the US Billboard 200.</p>
-
-            <h4>Singles</h4>
-            <ul>
-                <li>"One Step Closer"</li>
-                <li>"Crawling"</li>
-                <li>"In the End"</li>
-            </ul>
-        </div>
-    </div>
-    <hr>
-    <!-- End Hybrid Theory -->
-
-    <!-- Meteora -->
-    <div class="album_sect">
+        <!-- Hybrid Theory -->
         <div class="row align-items-center">
             <div class="column col-md-4 col-sm-4 col-xs-12">
-
-                <!-- Image from https://en.wikipedia.org/wiki/Meteora_(album) -->
-                <img src="images/meteora.jpg" alt="Meteora_Art">
+                <!-- Image from https://en.wikipedia.org/wiki/Hybrid_Theory -->
+                <img src="images/hybrid_theory.jpg" alt="Hybrid_Thoery_Art">
             </div>
 
             <div class="column col-md-8 col-sm-8 col-xs-12">
-                <h3>Meteora</h3>
-                <p>Meteora is the second studio album by American rock band Linkin Park. It was released on March 25, 2003 through Warner Bros. Records. The album was produced by the band alongside Don Gilmore.</p>
+                <h3>Hybrid Theory</h3>
+                <p>Hybrid Theory is the debut studio album by American rock band Linkin Park, released on October 24, 2000 through Warner Bros. Records. As of 2017, the album has been certified diamond by the RIAA for sales in the United States of over eleven million units, and peaking at number two on the US Billboard 200.</p>
 
                 <h4>Singles</h4>
                 <ul>
-                    <li>"Somewhere I belong"</li>
-                    <li>"Faint"</li>
-                    <li>"Numb"</li>
+                    <li>"One Step Closer"</li>
+                    <audio controls>
+  <source src="audio/Linkin%20Park%20-%20One%20Step%20Closer.mp3" type="audio/mpeg">
+  Your browser does not support the audio tag.
+</audio>
+                    <!--<p>Play icon on a button:
+        <button type="button" class="btn btn-default btn-sm"> 
+          <span class="glyphicon glyphicon-play"></span>Play
+        </button>
+      </p>-->
+                    <li>"Crawling"</li>
+                    <li>"In the End"</li>
                 </ul>
             </div>
         </div>
+        <hr>
+        <!-- End Hybrid Theory -->
+
+        <!-- Meteora -->
+        <div class="album_sect">
+            <div class="row align-items-center">
+                <div class="column col-md-4 col-sm-4 col-xs-12">
+
+                    <!-- Image from https://en.wikipedia.org/wiki/Meteora_(album) -->
+                    <img src="images/meteora.jpg" alt="Meteora_Art">
+                </div>
+
+                <div class="column col-md-8 col-sm-8 col-xs-12">
+                    <h3>Meteora</h3>
+                    <p>Meteora is the second studio album by American rock band Linkin Park. It was released on March 25, 2003 through Warner Bros. Records. The album was produced by the band alongside Don Gilmore.</p>
+
+                    <h4>Singles</h4>
+                    <ul>
+                        <li>"Somewhere I belong"</li>
+                        <li>"Faint"</li>
+                        <li>"Numb"</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <!-- End Meteora  -->
+
+
+        <!-- Minutes to Midnight -->
+        <div class="row align-items-center">
+            <div class="column col-md-4 col-sm-4 col-xs-12">
+                <!-- Image From https://en.wikipedia.org/wiki/Minutes_to_Midnight_(Linkin_Park_album) -->
+                <img src="images/minutes_to_midnight.jpg" alt="Minutes_to_Midnight_Art">
+            </div>
+
+            <div class="column col-md-8 col-sm-8 col-xs-12">
+                <h3>Minutes to Midnight</h3>
+                <p>Minutes to Midnight is the third studio album by American rock band Linkin Park, released on May 14, 2007, through Warner Bros. Records. The album was produced by Mike Shinoda and Rick Rubin.</p>
+
+                <h4>Singles</h4>
+                <ul>
+                    <li>"What I've Done"</li>
+                    <li>"Bleed It Out"</li>
+                    <li>"Shadow of the Day"</li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <!-- End Minutes to Midnight -->
+
+        <!-- A Thousand Suns -->
+        <div class="row align-items-center">
+            <div class="column col-md-4 col-sm-4 col-xs-12">
+
+                <!-- Image From https://en.wikipedia.org/wiki/A_Thousand_Suns -->
+                <img src="images/a_thousand_suns.jpg" alt="A_Thousand_Suns_Art">
+            </div>
+
+            <div class="column col-md-8 col-sm-8 col-xs-12">
+                <h3>A Thousand Suns</h3>
+                <p>A Thousand Suns is the fourth studio album by American rock band Linkin Park. It was released on September 8, 2010, by Warner Bros. Records. The album was written by the band and was produced by Linkin Park vocalist Mike Shinoda and Rick Rubin, who worked together to produce the band's previous studio album Minutes to Midnight (2007).</p>
+
+                <h4>Singles</h4>
+                <ul>
+                    <li>"The Catalyst"</li>
+                    <li>"Waiting for the End"</li>
+                    <li>"Burning in the Skies"</li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <!-- End A Thousand Suns -->
+
+        <!-- Living Things -->
+        <div class="row align-items-center">
+            <div class="column col-md-4 col-sm-4 col-xs-12">
+
+                <!-- Image From https://en.wikipedia.org/wiki/Living_Things_(Linkin_Park_album) -->
+                <img src="images/living_things.jpg" alt="Living_Things_Art">
+            </div>
+
+            <div class="column col-md-8 col-sm-8 col-xs-12">
+                <h3>Living Things</h3>
+                <p>Living Things is the fifth studio album by American rock band Linkin Park. It was released under Warner Bros. Records and Machine Shop Recordings on June 20, 2012, in Japan, and throughout the rest of the world during the following week. Production was handled by vocalist Mike Shinoda and Rick Rubin.</p>
+
+                <h4>Singles</h4>
+                <ul>
+                    <li>"Burn it Down"</li>
+                    <li>"Lost in the Echo"</li>
+                    <li>"Powerless"</li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <!-- End Living Things -->
+
+        <!-- The Hunting Party -->
+
+        <div class="row align-items-center">
+            <div class="column col-md-4 col-sm-4 col-xs-12">
+                <!-- Image From https://en.wikipedia.org/wiki/The_Hunting_Party_(album) -->
+                <img src="images/the_hunting_party.jpg" alt="The_Hunting_Party_Art">
+            </div>
+
+            <div class="column col-md-8 col-sm-8 col-xs-12">
+                <h3>The Hunting Party</h3>
+                <p>The Hunting Party is the sixth studio album by American rock band Linkin Park. The album, produced by band members Mike Shinoda and Brad Delson, was released by Warner Bros. Records and Machine Shop on June 13, 2014. It is the first album since Meteora (2003) not to be produced with Rick Rubin, after producing the band's previous three studio albums.</p>
+
+                <h4>Singles</h4>
+                <ul>
+                    <li>"Guitly All the Same"</li>
+                    <li>"Until It's Gone"</li>
+                    <li>"Wastelands"</li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <!-- End The Hunting Party -->
+
+        <!-- One More Light -->
+
+        <div class="row align-items-center">
+            <div class="column col-md-4 col-sm-4 col-xs-12">
+                <!-- Image From https://en.wikipedia.org/wiki/One_More_Light -->
+                <img src="images/one_more_light.jpeg" alt="One_More_Light_Art">
+            </div>
+
+            <div class="column col-md-8 col-sm-8 col-xs-12">
+                <h3>One More Light</h3>
+                <p>One More Light is the seventh studio album by American rock band Linkin Park. It was released on May 19, 2017 through Warner Bros. and Machine Shop. It is the last Linkin Park record produced before lead vocalist Chester Bennington's death on July 20, 2017.</p>
+
+                <h4>Singles</h4>
+                <ul>
+                    <li>"Heavy"</li>
+                    <li>"Talking to Myself"</li>
+                    <li>"One More Light"</li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <!-- End One More Light -->
     </div>
-    <hr>
-    <!-- End Meteora  -->
 
+    <!-- Start Form -->
+    <div class="survey" id="survey">
+        <form method="POST" action="survey-processor.php">
 
-    <!-- Minutes to Midnight -->
-    <div class="row align-items-center">
-        <div class="column col-md-4 col-sm-4 col-xs-12">
-            <!-- Image From https://en.wikipedia.org/wiki/Minutes_to_Midnight_(Linkin_Park_album) -->
-            <img src="images/minutes_to_midnight.jpg" alt="Minutes_to_Midnight_Art">
-        </div>
-
-        <div class="column col-md-8 col-sm-8 col-xs-12">
-            <h3>Minutes to Midnight</h3>
-            <p>Minutes to Midnight is the third studio album by American rock band Linkin Park, released on May 14, 2007, through Warner Bros. Records. The album was produced by Mike Shinoda and Rick Rubin.</p>
-
-            <h4>Singles</h4>
-            <ul>
-                <li>"What I've Done"</li>
-                <li>"Bleed It Out"</li>
-                <li>"Shadow of the Day"</li>
-            </ul>
-        </div>
-    </div>
-    <hr>
-    <!-- End Minutes to Midnight -->
-
-    <!-- A Thousand Suns -->
-    <div class="row align-items-center">
-        <div class="column col-md-4 col-sm-4 col-xs-12">
-
-            <!-- Image From https://en.wikipedia.org/wiki/A_Thousand_Suns -->
-            <img src="images/a_thousand_suns.jpg" alt="A_Thousand_Suns_Art">
-        </div>
-
-        <div class="column col-md-8 col-sm-8 col-xs-12">
-            <h3>A Thousand Suns</h3>
-            <p>A Thousand Suns is the fourth studio album by American rock band Linkin Park. It was released on September 8, 2010, by Warner Bros. Records. The album was written by the band and was produced by Linkin Park vocalist Mike Shinoda and Rick Rubin, who worked together to produce the band's previous studio album Minutes to Midnight (2007).</p>
-
-            <h4>Singles</h4>
-            <ul>
-                <li>"The Catalyst"</li>
-                <li>"Waiting for the End"</li>
-                <li>"Burning in the Skies"</li>
-            </ul>
-        </div>
-    </div>
-    <hr>
-    <!-- End A Thousand Suns -->
-
-    <!-- Living Things -->
-    <div class="row align-items-center">
-        <div class="column col-md-4 col-sm-4 col-xs-12">
-
-            <!-- Image From https://en.wikipedia.org/wiki/Living_Things_(Linkin_Park_album) -->
-            <img src="images/living_things.jpg" alt="Living_Things_Art">
-        </div>
-
-        <div class="column col-md-8 col-sm-8 col-xs-12">
-            <h3>Living Things</h3>
-            <p>Living Things is the fifth studio album by American rock band Linkin Park. It was released under Warner Bros. Records and Machine Shop Recordings on June 20, 2012, in Japan, and throughout the rest of the world during the following week. Production was handled by vocalist Mike Shinoda and Rick Rubin.</p>
-
-            <h4>Singles</h4>
-            <ul>
-                <li>"Burn it Down"</li>
-                <li>"Lost in the Echo"</li>
-                <li>"Powerless"</li>
-            </ul>
-        </div>
-    </div>
-    <hr>
-    <!-- End Living Things -->
-
-    <!-- The Hunting Party -->
-
-    <div class="row align-items-center">
-        <div class="column col-md-4 col-sm-4 col-xs-12">
-            <!-- Image From https://en.wikipedia.org/wiki/The_Hunting_Party_(album) -->
-            <img src="images/the_hunting_party.jpg" alt="The_Hunting_Party_Art">
-        </div>
-
-        <div class="column col-md-8 col-sm-8 col-xs-12">
-            <h3>The Hunting Party</h3>
-            <p>The Hunting Party is the sixth studio album by American rock band Linkin Park. The album, produced by band members Mike Shinoda and Brad Delson, was released by Warner Bros. Records and Machine Shop on June 13, 2014. It is the first album since Meteora (2003) not to be produced with Rick Rubin, after producing the band's previous three studio albums.</p>
-
-            <h4>Singles</h4>
-            <ul>
-                <li>"Guitly All the Same"</li>
-                <li>"Until It's Gone"</li>
-                <li>"Wastelands"</li>
-            </ul>
-        </div>
-    </div>
-    <hr>
-    <!-- End The Hunting Party -->
-
-    <!-- One More Light -->
-
-    <div class="row align-items-center">
-        <div class="column col-md-4 col-sm-4 col-xs-12">
-            <!-- Image From https://en.wikipedia.org/wiki/One_More_Light -->
-            <img src="images/one_more_light.jpeg" alt="One_More_Light_Art">
-        </div>
-
-        <div class="column col-md-8 col-sm-8 col-xs-12">
-            <h3>One More Light</h3>
-            <p>One More Light is the seventh studio album by American rock band Linkin Park. It was released on May 19, 2017 through Warner Bros. and Machine Shop. It is the last Linkin Park record produced before lead vocalist Chester Bennington's death on July 20, 2017.</p>
-
-            <h4>Singles</h4>
-            <ul>
-                <li>"Heavy"</li>
-                <li>"Talking to Myself"</li>
-                <li>"One More Light"</li>
-            </ul>
-        </div>
-    </div>
-    <hr>
-    <!-- End One More Light -->
-</div>
-
-<!-- Start Form -->
-<div class="survey" id="survey">
-    <form method="POST" action="survey-processor.php">
-
-        <?php
+            <?php
         include "inc/survey.inc";
         ?>
 
-    </form>
-</div>
-<!-- End Form -->
+        </form>
+    </div>
+    <!-- End Form -->
 
-<?php
+    <?php
 include "inc/bottom.inc";
 ?>
